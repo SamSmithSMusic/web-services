a5b91713308b2846b9c645dc57e64f994f6ad453
Samuel Smith <samsmithsmusic@gmail.com>
2025-09-17 18:32:31 -0600

    module update

---
+++ controllers/lesson1.js
@@ -0,0 +1,3 @@
+export const sam = (req,res) => {res.send("Samuel Smith");};
+
+export const wife = (req,res) => {res.send("Maddie Smith");};
--- server.js
+++ server.js
@@ -1,9 +1,10 @@
-const express = require('express');
+import { sam, wife } from "./controllers/lesson1.js";
+import express from "express";
+
 const app = express();
  
-app.get('/', (req, res) => {
-  res.send("Samuel Smith");
-});
+app.get('/', sam);
+app.get('/wife', wife);
  
 app.listen(process.env.PORT || 3000, () => {
   console.log('Web Server is listening at port ' + (process.env.PORT || 3000));
